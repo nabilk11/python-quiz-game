e4ab329ae8cb55206e2f7f4e8cd80835f583cd37
nabilk11 <khan.nabiln11@gmail.com>
2022-03-13 20:54:17 -0400

    added bonus round question

--- quiz_game.py
+++ quiz_game.py
@@ -1,4 +1,6 @@
 # Computer Quiz Game
+
+# WELCOME + START INPUT
 print("******** Welcome to the Computer Quiz ********")
 
 playing = input("Would you like to play? ").lower()
@@ -10,6 +12,7 @@ if playing != "yes": # if true (anything but yes, will quit game
 print("Then Let's Play!")
 score = 0
 
+# QUESTIONS
 answer = input("What does CPU stand for? ").lower()
 if answer == "central processing unit":
     print("Correct!")
@@ -28,13 +31,27 @@ if answer == "random access memory":
     score += 1
 else:
     print("Wrong :(")
-answer = input("What does PSU? ").lower()
+answer = input("What does PSU stand for? ").lower()
 if answer == "power supply unit":
     print("Correct!")
     score += 1
 else:
     print("Wrong :(")
 
-# Score Results
-print("You got " + str(score) + " questions correct!")
-print("That's " + str(score/4 * 100) + "%!")
+# BONUS ROUND
+print("Here Comes the Bonus Question! 2pts if You're Correct, -1pt if You're Wrong!")
+answer = input("What does SSD stand for? ").lower()
+if answer == "solid state drive":
+    print("Correct! 2pts added to your score!")
+    score += 2
+else:
+    print("Wrong, -1pt from your score :(")
+    score -= 1
+
+# SCORE RESULTS
+print("You're total score was " + str(score) + "pts!")
+
+if score == 6:
+    print("A Perfect Score of " + str(score/4 * 100) + "%!")
+else:
+    print("That's " + str(score/4 * 100) + "%!")
