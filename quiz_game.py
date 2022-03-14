f93bb6252fb74ac5187120190f6ac2cf4b52eab6
nabilk11 <khan.nabiln11@gmail.com>
2022-03-13 20:36:28 -0400

    completed v1, with score and % score

--- quiz_game.py
+++ quiz_game.py
@@ -8,24 +8,33 @@ if playing != "yes": # if true (anything but yes, will quit game
     quit() 
 
 print("Then Let's Play!")
+score = 0
 
 answer = input("What does CPU stand for? ").lower()
 if answer == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Wrong :(")
 answer = input("What does GPU stand for? ").lower()
 if answer == "graphics processing unit" or answer == "graphic processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Wrong :(")
 answer = input("What does RAM stand for? ").lower()
 if answer == "random access memory":
     print("Correct!")
+    score += 1
 else:
     print("Wrong :(")
 answer = input("What does PSU? ").lower()
 if answer == "power supply unit":
     print("Correct!")
+    score += 1
 else:
     print("Wrong :(")
+
+# Score Results
+print("You got " + str(score) + " questions correct!")
+print("That's " + str(score/4 * 100) + "%!")
